0108b914f45f3fc6d61befe93cacba08d2941b62
chockslam <64290122+chockslam@users.noreply.github.com>
2022-07-09 14:07:07 +0100

    Implementation of grabber and mover components.

---
+++ Source/CryptRaider/Mover.cpp
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Mover.h"
+#include "Math/UnrealMathUtility.h"
+
+// Sets default values for this component's properties
+UMover::UMover()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UMover::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	initLoc = GetOwner()->GetActorLocation();
+	targetLoc = initLoc + moveOffset; 
+	// ...
+	
+}
+
+
+// Called every frame
+void UMover::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if(shouldMove){
+		FVector currLoc = GetOwner()->GetActorLocation();
+		float Speed = FVector::Distance(initLoc, targetLoc) / moveTime;
+
+		FVector newLoc = FMath::VInterpConstantTo( currLoc, targetLoc, DeltaTime, Speed );
+
+		GetOwner()->SetActorLocation(newLoc);
+	}
+	
+	
+}
+
